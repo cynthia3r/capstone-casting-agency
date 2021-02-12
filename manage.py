6b7f50462f4fb3b71b017ebd862e1d574fe8f307
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-02-13 02:06:57 +1100

    chore: added seed data to populate db

--- manage.py
+++ manage.py
@@ -12,6 +12,22 @@ manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 
+# seed data prepared for database loading
+@manager.command
+def seed():
+    Movie(title='Wonder Woman 1984', release_date='2020-12-16').insert()
+    Movie(title='Space Sweepers', release_date='2021-02-05').insert()
+    Movie(title='Star Wars: The Rise of Skywalker (Episode IX)', release_date='2019-12-19').insert()
+    Movie(title='The Matrix Reloaded', release_date='2003-05-15').insert()
+    Movie(title='Parasite', release_date='2019-05-21').insert()
+
+    Actor(name='Tom Cruise', gender='male', age=58).insert()
+    Actor(name='Angelina Jolie', gender='female', age=45).insert()
+    Actor(name='Tom Hanks', gender='male', age=64).insert()
+    Actor(name='Meryl Streep', gender='female', age=71).insert()
+    Actor(name='Nicole Kidman', gender='female', age=53).insert()
+
+
 if __name__ == '__main__':
     manager.run()
 
