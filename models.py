9a45442875121b86e3a4cdac8ab53f30f4c1953c
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-02-15 14:53:34 +1100

    feat: added db_drop_and_create_all() for unittest purpose

--- models.py
+++ models.py
@@ -20,7 +20,20 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    #db.create_all()
+    # db.create_all()
+
+
+'''
+db_drop_and_create_all()
+    drops the database tables and starts fresh
+    can be used to initialize a clean database
+    !!NOTE you can change the database_filename variable to have multiple verisons of a database
+'''
+
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
 
 
 '''
